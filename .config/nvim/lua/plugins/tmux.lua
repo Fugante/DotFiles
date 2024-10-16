1267453238a3d19755cedf866f8457e52a541047
Fugante <ignisacedia@hotmail.com>
2024-10-16 16:47:24 -0600

    add tmux configuration files

---
+++ .config/nvim/lua/plugins/tmux.lua
@@ -0,0 +1,9 @@
+return {
+    "christoomey/vim-tmux-navigator",
+    keys = {
+        { "<C-h>", "<cmd>TmuxNavigateLeft<cr>" },
+        { "<C-l>", "<cmd>TmuxNavigateRight<cr>" },
+        { "<C-j>", "<cmd>TmuxNavigateDown<cr>" },
+        { "<C-k>", "<cmd>TmuxNavigateUp<cr>" },
+    },
+}
